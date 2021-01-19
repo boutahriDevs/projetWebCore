a9ef28244e170edccd5150d01824c06fdc8c7f57
BoutahriDevs <zak.stgr5@gmail.com>
2021-01-19 11:37:28 +0100

    Revert "Modification 91-01"
    This reverts commit 3de82231f5ed8e3152af24055a4b486188896738.

--- projetWebCore/Controllers/HomeController.cs
+++ projetWebCore/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ namespace projetWebCore.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-           /// modification sur la branche Branch_dev_asp 19-01-2021
+           /// modification sur la branche Branch_dev_asp
+           /// bonne utilisation de git sur visual studio
             _logger = logger;
         }
 
